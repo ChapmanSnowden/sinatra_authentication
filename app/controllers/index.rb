bbcc484e2f54235bae7bf6f2b67f8ac0b0a230ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-13 18:02:18 -0700

    initial commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,31 @@
+get '/' do
+  # render home page
+ #TODO: Show all users if user is signed in
+  erb :index
+end
+
+#----------- SESSIONS -----------
+
+get '/sessions/new' do
+  # render sign-in page 
+  erb :sign_in
+end
+
+post '/sessions' do
+  # sign-in
+end
+
+delete '/sessions/:id' do
+  # sign-out -- invoked via AJAX
+end
+
+#----------- USERS -----------
+
+get '/users/new' do
+  # render sign-up page
+  erb :sign_up
+end
+
+post '/users' do
+  # sign-up a new user
+end
